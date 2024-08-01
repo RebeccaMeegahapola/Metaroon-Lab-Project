8e13e8afd82554ea3e9f61419ad2c5c51c208d41
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-08-01 12:14:40 +0530

    up

--- shopping-cart/src/context/ShoppingCartContext.tsx
+++ shopping-cart/src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -10,10 +11,14 @@ type CartItem = {
 }
 
 type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[]
 }
 
 //create context
@@ -26,7 +31,17 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 
-    const [cartItems, setCartItems] = useState<CartItem[]>({})
+    const [isOpen, setIsOpen] = useState(false)
+    const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    //total quantity of items in a shopping cart
+    const cartQuantity = cartItems.reduce(
+        (quantity, item) => item.quantity + quantity,
+        0
+    )
+
+    const openCart = () => setIsOpen(true)
+    const closeCart = () => setIsOpen(false)
 
     //checks if the id of the current item in the array matches the id provided to the getItemQuantity function. If a match is found, .find() returns the item object. If no match is found, .find() returns undefined.
     function getItemQuantity(id: number) {
@@ -53,9 +68,36 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         })
     }
 
+    function decreaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+            //This line checks if the item with the specified id is already in the cart and check if the item has a quantity of 1
+            if (currItems.find(item => item.id === id)?.quantity === 1) {
+                //This removes the item from the cart and returns a new array that includes all items except the one with the specified id.
+                return currItems.filter(item => item.id !== id)
+            } else { //If the item is already in the cart/ The .map() function creates a new array by iterating over each item in currItems.
+                return currItems.map(item => {   
+                    //This checks if the current item's id matches the id of the item we're trying to decrease.             
+                    if (item.id === id) {
+                        //If the item matches, quantity increased by 1 [...item - spread operator - copies of properties]
+                        return { ...item, quantity: item.quantity - 1 }
+                    } else { //If the item does not match, return item unchanged
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
+    function removeFromCart(id: number) {
+        setCartItems(currItems => {
+            return currItems.filter(item => item.id !== id)
+        })
+    }
+
     return (
-        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity}}>
+        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity, openCart, closeCart}}>
             {children}
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     )
 }
